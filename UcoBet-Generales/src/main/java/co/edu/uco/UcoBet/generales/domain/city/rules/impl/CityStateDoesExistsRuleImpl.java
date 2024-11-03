51ce9b6a75c58714617cfcdda1c1b1727db1057e
simoncardenasramirez <simoncardenasramirez41@gmail.com>
2024-11-03 16:20:59 -0500

    Descripción del cambio

--- UcoBet-Generales/src/main/java/co/edu/uco/UcoBet/generales/domain/city/rules/impl/CityStateDoesExistsRuleImpl.java
+++ UcoBet-Generales/src/main/java/co/edu/uco/UcoBet/generales/domain/city/rules/impl/CityStateDoesExistsRuleImpl.java
@@ -1,9 +1,6 @@
 package co.edu.uco.UcoBet.generales.domain.city.rules.impl;
 
-import org.springframework.stereotype.Service;
 
-import co.edu.uco.UcoBet.generales.domain.city.rules.CityStateDoesExistsRule;
-import co.edu.uco.UcoBet.generales.domain.state.StateDomain;
 
 @Service
 public class CityStateDoesExistsRuleImpl implements CityStateDoesExistsRule{
--- UcoBet-Generales/src/main/java/co/edu/uco/UcoBet/generales/infraestructure/primaryadapters/controller/response/CityResponse.java
+++ UcoBet-Generales/src/main/java/co/edu/uco/UcoBet/generales/infraestructure/primaryadapters/controller/response/CityResponse.java
@@ -2,7 +2,7 @@ package co.edu.uco.UcoBet.generales.infraestructure.primaryadapters.controller.r
 
 import java.util.ArrayList;
 
-import co.edu.uco.UcoBet.generales.application.primaryports.dto.city.RegisterNewCityDto;
+
 
 public class CityResponse extends Response<RegisterNewCityDto> {
 
